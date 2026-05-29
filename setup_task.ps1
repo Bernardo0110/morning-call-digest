4d0baf3032db39c77a48cb4700e0e105cdbddba3
Bernardo Lima <bernardo.mrlima0110@gmail.com>
2026-05-29 00:02:56 -0300

    feat: Update scheduled task times for Morning Call Digest automation

--- setup_task.ps1
+++ setup_task.ps1
@@ -18,7 +18,7 @@ $wakeAction   = New-ScheduledTaskAction `
 $wakeTrigger  = New-ScheduledTaskTrigger `
     -Weekly `
     -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday `
-    -At "09:55AM"
+    -At "10:25AM"
 
 $wakeSettings = New-ScheduledTaskSettingsSet `
     -WakeToRun `
@@ -36,7 +36,7 @@ Register-ScheduledTask `
     -InputObject $wakeTask `
     -Force | Out-Null
 
-Write-Host "Tarefa de acordar criada: MorningCallDigest_Wake (09:55, seg-sex)"
+Write-Host "Tarefa de acordar criada: MorningCallDigest_Wake (10:25, seg-sex)"
 
 # ---------- Tarefa 2: roda o script ao meio-dia ----------
 $runAction   = New-ScheduledTaskAction `
@@ -47,13 +47,12 @@ $runAction   = New-ScheduledTaskAction `
 $runTrigger  = New-ScheduledTaskTrigger `
     -Weekly `
     -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday `
-    -At "10:00AM"
+    -At "10:30AM"
 
 $runSettings = New-ScheduledTaskSettingsSet `
     -WakeToRun `
     -ExecutionTimeLimit (New-TimeSpan -Minutes 30) `
-    -MultipleInstances IgnoreNew `
-    -StartWhenAvailable
+    -MultipleInstances IgnoreNew
 
 $runPrincipal = New-ScheduledTaskPrincipal `
     -UserId ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name) `
@@ -72,7 +71,7 @@ Register-ScheduledTask `
     -InputObject $runTask `
     -Force | Out-Null
 
-Write-Host "Tarefa principal criada:  MorningCallDigest_Run  (10:00, seg-sex)"
+Write-Host "Tarefa principal criada:  MorningCallDigest_Run  (10:30, seg-sex)"
 Write-Host ""
 Write-Host "Configuracao concluida. Verifique no Task Scheduler (taskschd.msc)."
 Write-Host "Para testar agora: Right-click em MorningCallDigest_Run > Run"
